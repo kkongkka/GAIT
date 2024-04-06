0bb4a5d92dc68b1aec1dae256df220721671c2f9
kkongkka <minji7380@kookmin.ac.kr>
2024-04-06 18:45:09 +0900

    first commit

--- gait_report.py
+++ gait_report.py
@@ -43,15 +43,10 @@ fp2_filtered.reset_index(inplace=True, drop=True)
 
 k_sr = 60; f_sr = 2160
 
-k_Heel_contact_1  = round(fp1_filtered['k_Heel_contact_1'][0])
-k_Toe_off_1       = round(fp1_filtered['k_Toe_off_1'][0])
-k_Heel_contact_2  = round(fp1_filtered['k_Heel_contact_2'][0])
-k_Toe_off_2       = round(fp1_filtered['k_Toe_off_2'][0])
-
-f_Heel_contact_1 = round(fp2_filtered['f_Heel_contact_1'][0])
-f_Toe_off_1      = round(fp2_filtered['f_Toe_off_1'][0])
-f_Heel_contact_2 = round(fp2_filtered['f_Heel_contact_2'][0])
-f_Toe_off_2      = round(fp2_filtered['f_Toe_off_2'][0])
+k_Heel_contact_1  = fp1_filtered[round(fp1_filtered['k_Heel_contact_1'][0],) == round(fp1_filtered['FRAMES'],2)].index.tolist()
+k_Toe_off_1       = fp1_filtered[round(fp1_filtered['k_Toe_off_1'][0],) == round(fp1_filtered['FRAMES'],2)].index.tolist()
+k_Heel_contact_2  = fp2_filtered[round(fp2_filtered['k_Heel_contact_2'][0],) == round(fp2_filtered['FRAMES'],2)].index.tolist()
+k_Toe_off_2       = fp2_filtered[round(fp2_filtered['k_Toe_off_2'][0],) == round(fp2_filtered['FRAMES'],2)].index.tolist()
 
 fp1_filtered.drop(['k_Heel_contact_1','k_Toe_off_1','k_Heel_contact_2','k_Toe_off_2','f_Heel_contact_1','f_Toe_off_1','f_Heel_contact_2','f_Toe_off_2'], axis=1, inplace=True)
 fp2_filtered.drop(['k_Heel_contact_1','k_Toe_off_1','k_Heel_contact_2','k_Toe_off_2','f_Heel_contact_1','f_Toe_off_1','f_Heel_contact_2','f_Toe_off_2'], axis=1, inplace=True)
@@ -64,21 +59,18 @@ fp2_len = len(fp2_filtered)
 fp2_time = np.arange(0,fp2_len/k_sr, 1/k_sr)
 fp_2time = fp2_time.round(3)
 
-# ===================================================================================
-# ============================= Using Data ==========================================
-
 ang_cols = {
     'PELVIS_ANGLE_X' : 'Pelvic_Anterior_Tilt',
-    '              ' : 'Pelvic_Up_(Obliquity)',
-    '              ' : 'Pelvic_Internal_Rotation',
+    #'              ' : 'Pelvic_Up_(Obliquity)',
+    #'              ' : 'Pelvic_Internal_Rotation',
     
     'RT_HIP_ANGLE_X' : 'Hip_Flexion',
     'RT_HIP_ANGLE_Y' : 'Hip_Adduction',
     'RT_HIP_ANGLE_Z' : 'Hip_Internal_Rotation',
     
     'RT_KNEE_ANGLE_X' : 'Knee_Flexion',
-    '               ' : 'Knee_Varus',
-    '               ' : 'Knee_Internal_Rotation',
+    #'               ' : 'Knee_Varus',
+    #'               ' : 'Knee_Internal_Rotation',
     
     'RT_ANKLE_ANGLE_X' : 'Ankle_Dorsiflexion',
     'RT_ANKLE_ANGLE_Y' : 'add',
@@ -87,22 +79,16 @@ ang_cols = {
     #'RT_ANKLE_ANGLE_Y' : 'Ankle_Rotation',
     }
 
-# ===================================================================================
-# ============================= DashBoard ===========================================
-st.title('KUM GAIT REPORT')
-
-# ============================ 그래프 함수 정의 =========================================
-
 def one_angle_plotly(data, cols, time, k_Heel_contact_1, k_Toe_off_1, k_Heel_contact_2, k_Toe_off_2):
     ang = {
         'max'       : {},
         'max_frame' : {},
         'min'       : {},
         'min_frame' : {},
-        'k_Heel_contact_1' : {},
-        'k_Toe_off_1'      : {},
-        'k_Heel_contact_2' : {},
-        'k_Toe_off_2'      : {},
+        'k_Heel_contact_1'   : {},
+        'k_Toe_off_1'        : {},
+        'k_Heel_contact_2'   : {},
+        'k_Toe_off_2'        : {},
     }
     
     figures = {}
@@ -118,24 +104,15 @@ def one_angle_plotly(data, cols, time, k_Heel_contact_1, k_Toe_off_1, k_Heel_con
         trace = go.Scatter(x=time, y=df, mode='lines', name=cols[col], line=dict(color='firebrick'))
         traces = [trace]
         
-        ang['k_Heel_contact_1'][col] = round(df['FRAMES'][0])
-        ang['k_Toe_off_1'][col]      = round(df['FRAMES'][100])
-    
+        ang['k_Heel_contact_1'][col] = round(df[k_Heel_contact_1])
+        ang['k_Toe_off_1'][col]      = round(df[k_Toe_off_1])
+        ang['k_Heel_contact_2'][col] = round(df[k_Heel_contact_2])
+        ang['k_Toe_off_2'][col]      = round(df[k_Toe_off_2])
         
         ang['max'][col]       = round(df.max(), 2)
         ang['max_frame'][col] = np.where(df == df.max())[0][0]
         ang['min'][col]       = round(df.min(), 2)
         ang['min_frame'][col] = np.where(df == df.min())[0][0]
-        
-        '''
-        if col in ['TORSO_ANGLE_Y','LEAD_ELBOW_ANGLE_X','LEAD_SHOULDER_ANGLE_Y','LEAD_SHOULDER_ANGLE_Z','LEAD_KNEE_ANGULAR_VELOCITY_X']:
-            ang['max'][col]  = round(df[k_fc_time-40:k_br_time+15].max(), 2)
-            ang['max_frame'][col] = np.where(df == df[k_fc_time-40:k_br_time+15].max())[0][0]
-
-        elif col in ['LEAD_KNEE_ANGLE_X']:
-            ang['max'][col]  = round(df[k_fc_time:k_br_time+1].max(), 2)
-            ang['max_frame'][col] = np.where(df == df[k_fc_time:k_br_time+1].max())[0][0]
-        '''
         
         reference_lines =[]
         annotations = []
@@ -143,13 +120,15 @@ def one_angle_plotly(data, cols, time, k_Heel_contact_1, k_Toe_off_1, k_Heel_con
         for key_time, description in zip([k_Heel_contact_1, k_Toe_off_1, k_Heel_contact_2, k_Toe_off_2],
                                      ['HC1', 'TO1', 'HC2', 'TO2']):
             
+            # 참조선
             reference_lines.append(
             go.Scatter(x=[time[key_time], time[key_time]], y=[df.min(), df.max()],
                        mode='lines', line=dict(color='black', width=2, dash='dash'),
                        showlegend=False)
         )
+            # 주석
             annotations.append(
-            dict(x=time[key_time + 2], y=0.95, xref='x', yref='paper', showarrow=False,
+            dict(x=time[key_time], y=0.95, xref='x', yref='paper', showarrow=False,
                  text=description,textangle=-90, bgcolor='rgba(0,0,0,0)', bordercolor='rgba(0,0,0,0)', borderwidth=0,
                  )
         )
@@ -169,7 +148,7 @@ def one_angle_plotly(data, cols, time, k_Heel_contact_1, k_Toe_off_1, k_Heel_con
                         ),                        
             showlegend=False,
             margin=dict(l=40, r=40, t=40, b=40),
-            height=600,
+            height=400,
             plot_bgcolor='rgb(43,48,61)',
             annotations=annotations
         )
@@ -182,6 +161,43 @@ def one_angle_plotly(data, cols, time, k_Heel_contact_1, k_Toe_off_1, k_Heel_con
         
     return ang, figures
 
-# ============================ 그래프 및 시점 수치 =======================================
+FP1_values, FP1_fig = one_angle_plotly(fp1_filtered, ang_cols, fp_1time, k_Heel_contact_1, k_Toe_off_1, k_Heel_contact_2, k_Toe_off_2)
+FP2_values, FP2_fig = one_angle_plotly(fp2_filtered, ang_cols, fp_2time, k_Heel_contact_1, k_Toe_off_1, k_Heel_contact_2, k_Toe_off_2)
+
+for col in FP1_fig:
+    fig = FP1_fig[col]
+    fig.update_layout(
+        width=500,  # Desired width
+        height=400,  # Desired height
+        autosize=False  # Disable autosizing to enforce provided dimensions
+    )
+
+for col in FP2_fig:
+    fig = FP2_fig[col]
+    fig.update_layout(
+        width=500,  # Desired width
+        height=400,  # Desired height
+        autosize=False  # Disable autosizing to enforce provided dimensions
+    )
+    
 st.header('분석 구간')
 st.image('image/analysis.png', use_column_width=True)
+
+st.subheader('KINEMATICS PARAMETERS')
+
+st.write("GROUND REACTION FORCE AP")
+
+for col in ang_cols:
+    st.write(ang_cols[col])
+
+    # Create two columns with equal width
+    col1, col2 = st.columns([1, 1])
+
+    with col1:
+        # Render the FP1_fig graph in the left column
+        st.plotly_chart(FP1_fig[col], use_container_width=True)
+
+    with col2:
+        # Render the FP2_fig graph in the right column
+        st.plotly_chart(FP2_fig[col], use_container_width=True)
+
